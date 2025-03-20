5a16c22d14695e6de4bcbdba0c37179ca2602a95
dilshadnirmal <dilshadgov2020@gmail.com>
2025-03-20 07:20:47 +0000

    updated gitignore

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-// const mongoose = require("mongoose");
-
-export const connectDB = async (MONGODB_URI) => {
-  await mongoose.connect(MONGODB_URI || "mongodb://localhost:27017/userDB");
-
-  mongoose.connection.on("connection", () =>
-    console.log(`connected to MONGODB`)
-  );
-};
