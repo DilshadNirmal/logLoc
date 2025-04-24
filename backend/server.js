f0714c9ef176ecc8f35d9995508b9a4408398920
dilshadnirmal <dilshadgov2020@gmail.com>
2025-04-24 07:10:32 +0000

    cors changes in server

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,27 @@ const dataRoutes = require("./routes/data");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      const allowedOrigins = [process.env.CLIENT_URL, process.env.CLIENT_URL_2];
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Authorization"],
+    maxAge: 3600,
+    preflightContinue: false,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
