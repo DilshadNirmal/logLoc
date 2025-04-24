be161fd2ecb338b48b42a82066057232a44c88ec
dilshadnirmal <dilshadgov2020@gmail.com>
2025-04-24 06:37:10 +0000

    changed ecosystem to run on dev

--- frontend/ecosystem.config.cjs
+++ frontend/ecosystem.config.cjs
@@ -1,16 +1,28 @@
 module.exports = {
-    apps: [{
-        name: "logLoc-frontend",
-        script: "serve",
-        args: "./start-server.sh",
-        instances: "1",
-        exec_mode: "fork",
-        env: {
-            PM2_SERVE_PATH: "./dist",
-            PM2_SERVE_PORT: 5173,
-            PM2_SERVE_SPA: "true",
-            NODE_ENV: "production",
-            HOST: "0.0.0.0"
-        }
-    }]
-}
+  apps: [
+    {
+      name: "logLoc-frontend",
+      // script: "serve",
+      // args: "./start-server.sh",
+      // instances: "1",
+      // exec_mode: "fork",
+      // env: {
+      //     PM2_SERVE_PATH: "./dist",
+      //     PM2_SERVE_PORT: 5173,
+      //     PM2_SERVE_SPA: "true",
+      //     NODE_ENV: "production",
+      //     HOST: "0.0.0.0"
+      // }
+
+      script: "npm",
+      args: "run dev",
+      interpreter: "none", // Don't use Node.js for npm/yarn
+      watch: true, // Optional: Restart on file changes
+      env: {
+        NODE_ENV: "development",
+        PORT: 5173, // Match Vite's default port
+        HOST: "0.0.0.0", // Allow LAN access
+      },
+    },
+  ],
+};
